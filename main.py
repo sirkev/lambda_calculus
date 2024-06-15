5e68829aa0723b8adf3b066d99630875c18bc410
kevin <kevinoduor8877@gmail.com>
2024-06-15 12:14:35 +0300

    handle multiple function inputs using curring

--- main.py
+++ main.py
@@ -5,10 +5,27 @@ def get_sum(num1, num2):
     total = num1 + num2
     print(f'sum:, {total}')
 
-
+#here, we make use of higher order funtions which enable us to take in functions as inputs and also return functions as outputs
 def l_sum(num1, num2):
+    #curring: this is a way of supporting multiple function inputs
+    #this done by having a function return another function sequentially performing a beta reduction for each of the inputs
     total = (lambda x: lambda y: x + y)(num1)(num2)
+
     print(f'sum using lambda:, {total}')
+
+
+
+#booleans and conditionals
+def if_then(b,x,y):
+    if b:
+        return x
+    else:
+        return y
+def l_bool(value):
+    true = (lambda x : lambda y: x)
+    false = (lambda x : lambda y: y)
+
+
 
 
 if __name__ == '__main__':
