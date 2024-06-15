f36d540a5895e1880c664dbfbbb23567cba9de45
kevin <kevinoduor8877@gmail.com>
2024-06-15 11:47:45 +0300

    handle multiple function inputs using curring

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # This is a sample Python script exploring lambda calculus concepts in programming
 
 
+def get_sum(num1, num2):
+    total = num1 + num2
+    print(f'sum:, {total}')
 
-def get_sum(name):
-    print(f'Hi, {name}')
+
+def l_sum(num1, num2):
+    total = (lambda x: lambda y: x + y)(num1)(num2)
+    print(f'sum using lambda:, {total}')
 
 
 if __name__ == '__main__':
-    get_sum('PyCharm')
-
+    get_sum(1, 2)
+    l_sum(1, 2)
