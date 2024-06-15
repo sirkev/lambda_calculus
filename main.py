a8f800e3e5dd2dc4e3dcbef9880994f83fcea6ec
kevin <kevinoduor8877@gmail.com>
2024-06-15 11:36:04 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script exploring lambda calculus concepts in programming
+
+
+
+def get_sum(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    get_sum('PyCharm')
+
